9bc39283e4cc6ab9e5913ccbf766998eab4ff093
Sergey Poznyakoff <gray@gnu.org.ua>
2010-03-01 10:49:03 +0200

    Bugfixes in rtapelib
    * lib/rmt.h (rmtcreat): Use fcntl O_ macros insead of
    their hardcoded values.
    * lib/rtapelib.c (rmt_read__,rmt_ioctl__): Prevent
    potential overflow.

--- lib/rtapelib.c
+++ lib/rtapelib.c
@@ -573,7 +573,8 @@ rmt_read__ (int handle, char *buffer, size_t length)
 
   sprintf (command_buffer, "R%lu\n", (unsigned long) length);
   if (do_command (handle, command_buffer) == -1
-      || (status = get_status (handle)) == SAFE_READ_ERROR)
+      || (status = get_status (handle)) == SAFE_READ_ERROR
+      || status > length)
     return SAFE_READ_ERROR;
 
   for (counter = 0; counter < status; counter += rlen, buffer += rlen)
@@ -709,6 +710,12 @@ rmt_ioctl__ (int handle, int operation, char *argument)
 	    || (status = get_status (handle), status == -1))
 	  return -1;
 
+	if (status > sizeof (struct mtop))
+	  {
+	    errno = EOVERFLOW;
+	    return -1;
+	  }
+	
 	for (; status > 0; status -= counter, argument += counter)
 	  {
 	    counter = safe_read (READ_SIDE (handle), argument, status);
