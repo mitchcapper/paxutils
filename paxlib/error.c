3dbb742377448e479858fc581ee9277a0c3e7970
Sergey Poznyakoff <gray@gnu.org>
2024-11-04 08:25:20 +0200

    Fix unclosed comments

--- paxlib/error.c
+++ paxlib/error.c
@@ -13,7 +13,7 @@
    Public License for more details.
 
    You should have received a copy of the GNU General Public License along
-   with this program.  If not, see <http://www.gnu.org/licenses/>.
+   with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <system.h>
 #include <paxlib.h>
